b31595d39b0f09b66b63348c4d402d51135477f9
woozxn <squib0612@gmail.com>
2022-08-09 18:31:43 +0900

    Initial commit

---
+++ src/main/java/com/woozxnlog/WoozxnlogApplication.java
@@ -0,0 +1,13 @@
+package com.woozxnlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoozxnlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoozxnlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woozxnlog/WoozxnlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woozxnlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoozxnlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
