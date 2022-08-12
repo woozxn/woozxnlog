692df5f699bedef14d22739e228bf28dc9789f98
woozxn <squib0612@gmail.com>
2022-08-12 17:15:39 +0900

    feat(woozxn) : 데이터 검증-1

--- src/main/java/com/woozxnlog/controller/PostController.java
+++ src/main/java/com/woozxnlog/controller/PostController.java
@@ -2,8 +2,13 @@ package com.woozxnlog.controller;
 
 import com.woozxnlog.request.PostCreate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -11,8 +16,18 @@ import java.util.Map;
 public class PostController {
 
     @PostMapping("/posts")
-    public String post(@RequestBody PostCreate params) {
-        log.info("params={}", params.toString());
-        return "Hello World";
+    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) {
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            FieldError firstFieldError = fieldErrors.get(0);
+            String fieldName = firstFieldError.getField(); // title
+            String errorMessage = firstFieldError.getDefaultMessage();// ..에러 메시지
+
+            Map<String, String> error = new HashMap<>();
+            error.put(fieldName, errorMessage);
+            return error;
+        }
+
+        return Map.of();
     }
 }
--- src/test/java/com/woozxnlog/controller/PostControllerTest.java
+++ src/test/java/com/woozxnlog/controller/PostControllerTest.java
@@ -28,7 +28,19 @@ class PostControllerTest {
                         .content("{\"title\": \"제목입니다.\", \"content\": \"내용입니다.\"}")
                 )
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"))
+                .andExpect(content().string("{}"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/posts 요청시 title값은 필수다.")
+    void test2() throws Exception {
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"\", \"content\": \"내용입니다.\"}")
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("타이틀을 입력해주세요."))
                 .andDo(print());
     }
 }
