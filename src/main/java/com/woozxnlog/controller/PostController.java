9d55682bc14bbfeb85799db1d04470ddee618941
woozxn <squib0612@gmail.com>
2022-08-18 20:53:43 +0900

    feat(woozxn) : 데이터 검증-1

--- src/main/java/com/woozxnlog/controller/PostController.java
+++ src/main/java/com/woozxnlog/controller/PostController.java
@@ -16,18 +16,7 @@ import java.util.Map;
 public class PostController {
 
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate params, BindingResult result) {
-        if (result.hasErrors()) {
-            List<FieldError> fieldErrors = result.getFieldErrors();
-            FieldError firstFieldError = fieldErrors.get(0);
-            String fieldName = firstFieldError.getField(); // title
-            String errorMessage = firstFieldError.getDefaultMessage();// ..에러 메시지
-
-            Map<String, String> error = new HashMap<>();
-            error.put(fieldName, errorMessage);
-            return error;
-        }
-
+    public Map<String, String> post(@RequestBody @Valid PostCreate params) {
         return Map.of();
     }
 }
