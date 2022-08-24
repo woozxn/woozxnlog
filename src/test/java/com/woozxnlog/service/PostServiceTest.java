2ce7c92c2a757945154f2ed94e507e7c8ce3751f
woozxn <squib0612@gmail.com>
2022-08-24 17:50:52 +0900

    test(woozxn) : PostService 테스트

---
+++ src/test/java/com/woozxnlog/service/PostServiceTest.java
@@ -0,0 +1,40 @@
+package com.woozxnlog.service;
+
+import com.woozxnlog.domain.Post;
+import com.woozxnlog.repository.PostRepository;
+import com.woozxnlog.request.PostCreate;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("글 작성")
+    void test1() {
+        //given
+        PostCreate postCreate = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+
+        //when
+        postService.write(postCreate);
+
+        //then
+        assertEquals(1L, postRepository.count());
+        Post post = postRepository.findAll().get(0);
+        assertEquals("제목입니다.", post.getTitle());
+        assertEquals("내용입니다.", post.getContent());
+    }
+}
